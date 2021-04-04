56e9ff1f9b915cffbe893e14512f0ed404f68b99
Joshua Athayde <joshua.athayde@gmail.com>
2021-04-03 22:15:01 -0500

    changed location of index

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 })
 
 app.use(express.static(__dirname + '/public'));
